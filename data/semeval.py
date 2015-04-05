a8a92f33d4e7160ef4bf75b8cca3772c15b9b77c
aconkey <adam.conkey@gmail.com>
2015-04-05 11:12:49 -0500

    processed causal semeval data

--- data/semeval.py
+++ data/semeval.py
@@ -2,6 +2,8 @@ import shutil
 import os
 import itertools
 import shlex
+import re
+import string
 
 """
 
@@ -19,13 +21,24 @@ def remove_blank_lines(filename):
     
 def get_causal_sents(filename):
     with open(filename, 'r') as in_file:
-        with open("causal_sents.txt", 'w') as out_file:
+        with open("temp.txt", 'w') as out_file:
             while True:
                 next3lines = list(itertools.islice(in_file, 3))
                 if not next3lines:
                     break
                 if "Cause-Effect" in next3lines[1]:
                     first_line = shlex.split(next3lines[0])
-                    out_file.write("1 " + first_line[1]) # positive classification + sentence
+                    out_file.write("1 " + first_line[1] + '\n') # positive classification + sentence
+    shutil.copyfile("temp.txt", filename)
+    os.remove("temp.txt")
                     
-                    # TODO: remove "e" tags from first_line
+def remove_tags(filename):
+    with open(filename, 'r') as in_file:
+        with open("temp.txt", 'w') as out_file:
+            for line in in_file:
+                line = re.sub(r'<.*?>', '', line)
+                line = line.translate(None, string.punctuation).lower()
+                out_file.write(line)
+    shutil.copyfile("temp.txt", filename)
+    os.remove("temp.txt")                
+                
