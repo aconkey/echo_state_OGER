f29475ad3e0b10b7849e7d5072ce603f6b8a8588
aconkey <adam.conkey@gmail.com>
2015-04-18 14:51:59 -0500

    Progress on ESN, not yet running

--- echostate.py
+++ echostate.py
@@ -4,27 +4,34 @@ import vectorize
 import random
 import numpy as np
 
-filename = sys.argv[1]
+#filename = sys.argv[1]
+filename = "data/semeval_combined.txt"
 x,y = vectorize.get_vectorized_data(filename) # x = sentence, y = classification
 
 # for now just do simple hack splitting of train/test sets
 # TODO more advanced cross validation
 ziplist = zip(x,y)
-random.shuffle(ziplist)
-train_data = [None, ziplist[:1800]]
-test_data = ziplist[1800:]
-test_inputs, test_targets = zip(*test_data) # get the input/output separation back
-test_inputs = np.asarray(test_inputs)
-test_targets = np.asarray(test_targets)
+#random.shuffle(ziplist)
+data = [x, ziplist]
+#train_data = [None, ziplist[:1800]]
+#test_data = ziplist[1800:]
+#test_inputs, test_targets = zip(*test_data) # get the input/output separation back
+#test_inputs = np.asarray(test_inputs)
+#test_targets = np.asarray(test_targets)
 
 reservoir = Oger.nodes.ReservoirNode(output_dim = 100)
 readout = Oger.nodes.RidgeRegressionNode()
-
+    
 flow = reservoir + readout
-flow.train(train_data)
 
-test_outputs = flow.execute(test_inputs)
-confmat = Oger.utils.ConfusionMatrix.from_data(2, test_outputs, test_targets)
+test_errors = Oger.evaluation.validate(
+    data, flow, Oger.utils.nmse, cross_validate_function=Oger.evaluation.train_test_only, training_fraction=0.5)
+        
+#flow.train(train_data)
+
+#test_outputs = flow.execute([None, test_inputs])
+#confmat = Oger.utils.ConfusionMatrix.from_data(2, test_outputs, test_targets)
 
 
-# TODO debug! this code doesn't work yet and is all a horrible hack
+# TODO getting divide by zero error, try comparing with privided datasets to 
+# see why it is not behaving like theirs
